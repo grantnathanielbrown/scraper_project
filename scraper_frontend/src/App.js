6bb5ff1e53241483e91db8d437fa3212c4ee26d7
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-07-08 19:01:13 -0400

    able to generate posts by using map

--- scraper_frontend/src/App.js
+++ scraper_frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import anime from 'animejs';
+import placeholder from './wsi-imageoptim-reddit-marketing-.jpg';
 import './App.css';
 
 export default class App extends Component {
@@ -7,15 +8,18 @@ export default class App extends Component {
     super(props)
     this.state= {
       categoryValue: "best",
+
       filters: "Filters",
       numPosts: "Number of Posts",
       searchTerm: "Search Term",
-      subreddit: "Subreddit"
+      subreddit: "Subreddit",
+      postArray: [],
     }  
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 }
 componentDidMount () {
+  // When the page opens, animate the title
   anime({
     targets: '.page-title',
     translateY: [100,0],
@@ -32,19 +36,33 @@ handleChange(event) {
 }
 
 handleSubmit(event) {
+  let that = this;
   console.log(this.state.categoryValue);
   fetch(`https://www.reddit.com/${this.state.categoryValue}.json`)
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
-    // console.log(JSON.stringify(myJson));
+    
+    let postArray = [];
     let parsedJSON = myJson;
     console.log(parsedJSON);
     console.log(parsedJSON.data.children[0].data.score);
-  });
+    for (let i = 0; i < 25; i++) {
+      let miniPost = {score: parsedJSON.data.children[i].data.score,
+      title: parsedJSON.data.children[i].data.title,
+      url: parsedJSON.data.children[i].data.url,
+      thumbnail: parsedJSON.data.children[i].data.thumbnail};
+      postArray.push(miniPost);
+    }
+    that.setState({postArray: postArray});
+    console.log(that.state.postArray);
+  })
+
   event.preventDefault();
 }
+
+
 // handleChange(event) {
 //   let x = event.target.className;
 //   console.log(x);
@@ -55,7 +73,26 @@ handleSubmit(event) {
 
 
   render() {
+    let posts = this.state.postArray.map( (post, key) => {
+    if (post.thumbnail != "self") {
+      return (
+        <div className="generated-post">
+          <img src={post.url} alt="thumbnail of a Reddit post"/>
+          <ul>{post.title}</ul>
+          <ul>{post.score}</ul>
+        </div>
+      )
+    } else {
+      return (
+        <div className="generated-post">
+          <img src={placeholder} alt="Reddit logo"/>
+          <ul>{post.title}</ul>
+          <ul>{post.score}</ul>
+        </div>        
+      )
+    }
 
+    })
     return (
       <div className="App">
         <h1 className="page-title">Scrappy</h1>
@@ -79,7 +116,7 @@ handleSubmit(event) {
           <input className="rounded subreddit" value={this.state.subreddit} onChange={this.handleChange} type="text"/>
           <input className="rounded" value="Go!" type="submit" />        
         </form>
-  
+      {posts}
       </div>
     );
   }
