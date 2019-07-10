beeb46418ba482f1882297bc3a53119ea6fd2eb0
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-07-10 17:44:55 -0400

    committing before i make some major front and changes

--- scraper_backend/app.js
+++ scraper_backend/app.js
@@ -13,12 +13,7 @@ app.use(function(req, res, next) {
 });
 
 
-const r = new snoowrap({
-    userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-    clientId: '38BBGLjgEX3i8A',
-    clientSecret: '3Ji_s4lJ4RzlXSIfxngu9WvTOfk',
-    refreshToken: '23280913-oFa2QT9FoJEU2mLC2r-WeyHsXN0'
-  });
+
 
 // for methods like getHot or getTop, returns an array of listings
 // 1. receive request from front en
