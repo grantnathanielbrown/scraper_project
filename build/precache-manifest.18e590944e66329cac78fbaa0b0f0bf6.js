a3df3daa8a6c6f0738925ab4529a658604df8ff8
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-08-11 15:34:10 -0400

    created an empty test file because i want to testify canp//

---
+++ build/precache-manifest.18e590944e66329cac78fbaa0b0f0bf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc47e37fd86fc5083cdde1ee6d57cc9",
+    "url": "/scraper_project/index.html"
+  },
+  {
+    "revision": "5fe47d4e71f4f6987c84",
+    "url": "/scraper_project/static/css/main.86dd63a3.chunk.css"
+  },
+  {
+    "revision": "b516ff664059532c39c2",
+    "url": "/scraper_project/static/js/2.5a57da10.chunk.js"
+  },
+  {
+    "revision": "5fe47d4e71f4f6987c84",
+    "url": "/scraper_project/static/js/main.2978f7d6.chunk.js"
+  },
+  {
+    "revision": "7dc3d68091e9e9e3b54d",
+    "url": "/scraper_project/static/js/runtime~main.d6dcd1d8.js"
+  },
+  {
+    "revision": "a171ce6d5892bf94aa58c13ec9902e50",
+    "url": "/scraper_project/static/media/wsi-imageoptim-reddit-marketing-.a171ce6d.jpg"
+  }
+]);
