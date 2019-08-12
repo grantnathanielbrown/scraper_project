7204b3177c9e9639c9c22e8b78300838989c920a
Grant Brown <grant@Grants-MacBook-Pro.local>
2019-08-11 22:08:23 -0400

    Updates

---
+++ precache-manifest.5036f380ee5f94ad16815527f6eb201f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf29d208a4101c7ac91142f122ab105",
+    "url": "/scraper_project/index.html"
+  },
+  {
+    "revision": "229f2c44d3455426b5d7",
+    "url": "/scraper_project/static/css/main.468054e3.chunk.css"
+  },
+  {
+    "revision": "771f01c799902cb17e92",
+    "url": "/scraper_project/static/js/2.741b94ac.chunk.js"
+  },
+  {
+    "revision": "229f2c44d3455426b5d7",
+    "url": "/scraper_project/static/js/main.721490e6.chunk.js"
+  },
+  {
+    "revision": "7dc3d68091e9e9e3b54d",
+    "url": "/scraper_project/static/js/runtime~main.d6dcd1d8.js"
+  },
+  {
+    "revision": "a171ce6d5892bf94aa58c13ec9902e50",
+    "url": "/scraper_project/static/media/wsi-imageoptim-reddit-marketing-.a171ce6d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5991e96b30fe70accbc7f926c36c0ee.js"
+  "/scraper_project/precache-manifest.5036f380ee5f94ad16815527f6eb201f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/scraper_project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
